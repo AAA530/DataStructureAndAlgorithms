8538d77a2f4711512f7ba5a5f190fbdf17b5e8ab
AAA530 <dhairyapatelad@gmail.com>
2023-12-09 01:07:32 -0500

    day 29 solved 1 question

---
+++ 94. Binary Tree Inorder Traversal.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    List<Integer> ans = new ArrayList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        inorder(root);
+        return ans;
+    }
+
+    public void inorder(TreeNode root) {
+        if(root==null){
+            return;
+        }
+
+        inorder(root.left);
+        ans.add(root.val);
+        inorder(root.right);
+    }
+
+
+}
