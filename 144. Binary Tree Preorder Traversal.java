76ff61ebee1b63916d639bce1a305ef7dd775897
AAA530 <dhairyapatelad@gmail.com>
2023-12-11 00:14:16 -0500

    day 31 solved 1 question

---
+++ 144. Binary Tree Preorder Traversal.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    List<Integer> ans = new ArrayList<>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+        preorder(root);
+        return ans;
+        
+    }
+
+    public void preorder(TreeNode root) {
+        if(root==null){
+            return;
+        }
+
+        ans.add(root.val);
+        preorder(root.left);
+        
+        preorder(root.right);
+    }
+}
